23ca65115b2e0c393775ab92f6e95fbd63840201
Francois Huyghe <francois@13milliseconds.com>
2018-07-18 16:35:39 -0400

    closing the other opened drawers

--- prod-bundle/public/modules/drawer-widgets/js/always.js
+++ prod-bundle/public/modules/drawer-widgets/js/always.js
@@ -3,10 +3,15 @@ apos.define('drawer-widgets', {
     construct: function(self, options) {
       self.play = function($widget, data, options) {
           //The actual functions
-        $widget.find('.title').click(function() {
-          $widget.find('.content').toggle();
-          $widget.find('.title i').toggleClass('fa-caret-down');
-          $widget.find('.title i').toggleClass('fa-caret-up');
+          $widget.click('.title', function(e) {
+          if ($widget.find('.drawer').hasClass('active')){
+            $('.drawer.active').removeClass('active')
+          } else {
+            $('.drawer.active').removeClass('active')
+            $widget.find('.drawer').addClass('active');
+          }
+          $widget.find('i').toggleClass('fa-caret-down');
+          $widget.find('i').toggleClass('fa-caret-up');
           // Stop bubbling and default behavior for jQuery event
           return false;
         });
